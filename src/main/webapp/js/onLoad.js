1677a5213682de7e393749c990d71e1c16a364f2
Jenny <jdommett@gmail.com>
2014-07-13 15:13:41 +0100

    start of instructionEvaluator to start parsing the instructions. Listens on keypress and logs lines

--- src/main/webapp/js/onLoad.js
+++ src/main/webapp/js/onLoad.js
@@ -4,9 +4,11 @@ $(document).ready(function()
     var chartRenderer = new ChartRenderer();
     var chartModel = new ChartModel(chartRenderer);
     var keyListener = new KeyListener();
+    var instructionEvaluator = new InstructionEvaluator();
 
     chartRenderer.initialiseCanvasRatio();
     keyListener.addListener(chartModel);
+    keyListener.addListener(instructionEvaluator);
 
     $("#instructions").keypress(function(event){
         keyListener.onKeyPressed(event);
