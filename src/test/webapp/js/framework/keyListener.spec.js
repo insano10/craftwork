183b8d18ac0dd65a720056dd11c793394cfcfc17
Jenny <jdommett@gmail.com>
2014-08-15 15:04:10 +0100

    fix keylistener tests

--- src/test/webapp/js/framework/keyListener.spec.js
+++ src/test/webapp/js/framework/keyListener.spec.js
@@ -17,7 +17,39 @@ define(["jquery", "keyListener"], function($, KeyListener)
             var stubEvent = {which:77};
             keyListener.onKeyPressed(stubEvent);
 
-            expect(stubListener.notifyNewInstructionCharacter).toHaveBeenCalledWith("M");
+            expect(stubListener.notifyNewInstructionCharacter).toHaveBeenCalled();
+        });
+
+        it("should not notify listeners of a key when down if it has not been explicitly set", function() {
+
+            var stubEvent = {which:77};
+            keyListener.onKeyDown(stubEvent);
+
+            expect(stubListener.notifyNewInstructionCharacter).not.toHaveBeenCalled();
+        });
+
+        it("should notify listeners of a key when down if it is backspace", function() {
+
+            var stubEvent = {which:8};
+            keyListener.onKeyDown(stubEvent);
+
+            expect(stubListener.notifyNewInstructionCharacter).toHaveBeenCalled();
+        });
+
+        it("should notify listeners of a key when down if it is ctrl+z", function() {
+
+            var stubEvent = {which:90, ctrlKey:true};
+            keyListener.onKeyDown(stubEvent);
+
+            expect(stubListener.notifyNewInstructionCharacter).toHaveBeenCalled();
+        });
+
+        it("should notify listeners of a key when down if it is ctrl+shift+z", function() {
+
+            var stubEvent = {which:90, ctrlKey:true, shiftKey:true};
+            keyListener.onKeyDown(stubEvent);
+
+            expect(stubListener.notifyNewInstructionCharacter).toHaveBeenCalled();
         });
 
     });
