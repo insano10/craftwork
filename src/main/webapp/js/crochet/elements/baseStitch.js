2ab42e8197ac95ed1a8a5968e2bd3ec71575e674
Jenny <jdommett@gmail.com>
2014-10-06 20:19:20 +0100

    actually no, don't do that

--- src/main/webapp/js/crochet/elements/baseStitch.js
+++ src/main/webapp/js/crochet/elements/baseStitch.js
@@ -274,29 +274,7 @@ define(["jquery", "stitchUtils", "renderedStitch"], function ($, StitchUtils, Re
 
         Stitch.prototype.notifyStitchAboveRenderingDataUpdated = function notifyStitchAboveRenderingDataUpdated(renderedStitchAbove, renderContext)
         {
-            if(renderedStitchAbove.getRenderAngle() != 0)
-            {
-                console.log(this.toString() + " is going to be modified as angle above is " + renderedStitchAbove.getRenderAngle());
-
-                var renderPosition =
-                {
-                    x: this.getRenderXPos(renderContext),
-                    y: this.getRenderYPos(renderContext)
-                };
-                var angleOfRotation = renderedStitchAbove.getRenderAngle();
-                var renderedStitch = new RenderedStitch(renderPosition, angleOfRotation, this.imgWidth, this.imgHeight, this.rowNum);
-                renderContext.addRenderedStitch(this.getId(), renderedStitch);
-
-                //update all subsequent stitches as this stitch has changed
-                if (this.nextStitch != null)
-                {
-                    this.nextStitch.populateRenderingData(renderContext, false);
-                }
-            }
-            else
-            {
-                console.log(this.toString() + " is going to be left alone as angle above is 0");
-            }
+            //no-op
         };
 
         Stitch.prototype.toString = function toString()
