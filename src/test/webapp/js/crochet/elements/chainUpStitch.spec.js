acbddab1a90e857f4b8b69729058b5c5361e4140
Jenny <jdommett@gmail.com>
2014-10-19 16:55:41 +0100

    fix ChainUpStitch unit tests

--- src/test/webapp/js/crochet/elements/chainUpStitch.spec.js
+++ src/test/webapp/js/crochet/elements/chainUpStitch.spec.js
@@ -1,28 +1,34 @@
 define(["jquery", "chainUpStitch", "singleStitch" ], function ($, ChainUpStitch, SingleStitch)
 {
 
-    xdescribe("ChainUpStitch", function ()
+    describe("ChainUpStitch", function ()
     {
         var chain = [];
         var tailOfChain;
 
+        function addStitchToChain(stitch, chain)
+        {
+            chain.push(stitch);
+            chain[chain.length - 2].setNextStitch(chain[chain.length - 1]);
+            stitch.connectToRowBelow(tailOfChain);
+            tailOfChain = stitch;
+        }
+
         beforeEach(function()
         {
             //create a chain of 10 sc
+            chain = [];
             chain.push(new SingleStitch("sc.png", 13, 13, 1));
             for(var i = 1; i<10 ; i++)
             {
-                chain.push(new SingleStitch("sc.png", 13, 13, 1));
-                chain[i-1].setNextStitch(chain[i]);
-                chain[i].setPreviousStitch(chain[i-1]);
+                addStitchToChain(new SingleStitch("sc.png", 13, 13, 1), chain);
             }
-            tailOfChain = chain[9];
         });
 
         it("should chain up 1", function ()
         {
-            var stitch = new ChainUpStitch("chain-up.png", 13, 13, 2);
-            stitch.connectToRowBelow(tailOfChain);
+            var stitch = new ChainUpStitch("chain-up.png", 13, 13, 1);
+            addStitchToChain(stitch, chain);
 
             //chain up does not take a connection from the stitch below
             expect(chain[9].isAvailableForConnection()).toBe(true);
@@ -32,27 +38,29 @@ define(["jquery", "chainUpStitch", "singleStitch" ], function ($, ChainUpStitch,
 
         it("should chain up 3", function ()
         {
-            var stitch = new ChainUpStitch("chain-up.png", 13, 13, 2);
-            var stitch2 = new ChainUpStitch("chain-up.png", 13, 13, 2);
-            var stitch3 = new ChainUpStitch("chain-up.png", 13, 13, 2);
-            stitch.connectToRowBelow(tailOfChain);
-            stitch2.connectToRowBelow(stitch);
-            stitch3.connectToRowBelow(stitch2);
+            var stitch = new ChainUpStitch("chain-up.png", 13, 13, 1);
+            var stitch2 = new ChainUpStitch("chain-up.png", 13, 13, 1);
+            var stitch3 = new ChainUpStitch("chain-up.png", 13, 13, 1);
+            addStitchToChain(stitch, chain);
+            addStitchToChain(stitch2, chain);
+            addStitchToChain(stitch3, chain);
 
             //chain up does not take a connection from the stitch below
             expect(chain[9].isAvailableForConnection()).toBe(true);
             expect(chain[9].getStitchesAbove().length).toEqual(0);
             expect(stitch.getStitchesBelow().length).toEqual(0);
+            expect(stitch2.getStitchesBelow().length).toEqual(0);
+            expect(stitch3.getStitchesBelow().length).toEqual(0);
         });
 
         it("should continue with stitches in first link of row after chain up", function ()
         {
-            var chainup = new ChainUpStitch("chain-up.png", 13, 13, 2);
-            chainup.connectToRowBelow(tailOfChain);
+            var chainup = new ChainUpStitch("chain-up.png", 13, 13, 1);
+            addStitchToChain(chainup, chain);
 
             var stitch = new SingleStitch("sc.png", 13, 13, 2);
-            stitch.connectToRowBelow(chainup);
-x
+            addStitchToChain(stitch, chain);
+
             expect(chain[9].isAvailableForConnection()).toBe(false);
             expect(chain[9].getStitchesAbove().length).toEqual(1);
             expect(stitch.getStitchesBelow().length).toEqual(1);
