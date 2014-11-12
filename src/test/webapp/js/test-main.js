62032f740fd7e664994fe5d6c7e1671fc0015298
Jenny <jdommett@gmail.com>
2014-11-12 20:41:43 +0000

    shuffle js into mvc folders and tidy up jquery import

--- src/test/webapp/js/test-main.js
+++ src/test/webapp/js/test-main.js
@@ -23,19 +23,19 @@ require.config(
         baseUrl: '/base',
 
         paths:    {
-            "jquery":            "src/test/webapp/js/requireJQuery",
-            "keyListener":       "src/main/webapp/js/framework/keyListener",
-            "instructionParser": "src/main/webapp/js/crochet/instructionParser",
-            "parseChainFactory": "src/main/webapp/js/crochet/parseChainFactory",
-            "stitchUtils":       "src/main/webapp/js/crochet/stitchUtils",
-            "renderedStitch":    "src/main/webapp/js/crochet/rendering/renderedStitch",
-            "renderContext":     "src/main/webapp/js/crochet/rendering/renderContext",
-            "baseStitch":        "src/main/webapp/js/crochet/elements/baseStitch",
-            "decreaseStitch":    "src/main/webapp/js/crochet/elements/decreaseStitch",
-            "increaseStitch":    "src/main/webapp/js/crochet/elements/increaseStitch",
-            "singleStitch":      "src/main/webapp/js/crochet/elements/singleStitch",
-            "chainStitch":       "src/main/webapp/js/crochet/elements/chainStitch",
-            "chainUpStitch":     "src/main/webapp/js/crochet/elements/chainUpStitch"
+            "jquery":            "src/main/webapp/js/lib/jquery-2.1.0.min",
+            "keyListener":       "src/main/webapp/js/controller/keyListener",
+            "instructionParser": "src/main/webapp/js/model/crochet/instructionParser",
+            "parseChainFactory": "src/main/webapp/js/model/crochet/parseChainFactory",
+            "stitchUtils":       "src/main/webapp/js/model/crochet/stitchUtils",
+            "renderedStitch":    "src/main/webapp/js/model/crochet/rendering/renderedStitch",
+            "renderContext":     "src/main/webapp/js/model/crochet/rendering/renderContext",
+            "baseStitch":        "src/main/webapp/js/model/crochet/elements/baseStitch",
+            "decreaseStitch":    "src/main/webapp/js/model/crochet/elements/decreaseStitch",
+            "increaseStitch":    "src/main/webapp/js/model/crochet/elements/increaseStitch",
+            "singleStitch":      "src/main/webapp/js/model/crochet/elements/singleStitch",
+            "chainStitch":       "src/main/webapp/js/model/crochet/elements/chainStitch",
+            "chainUpStitch":     "src/main/webapp/js/model/crochet/elements/chainUpStitch"
         },
 
         // dynamically load all test files
